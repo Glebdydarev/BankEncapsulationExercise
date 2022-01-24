b8e2bc95337ca3ae3a46d69ee16c63b61357e75d
Gleb Dudarev <glebdydarev@mail.ru>
2022-01-24 10:51:13 -0600

    done

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+        
+    {
+       
+        public BankAccount(double amount)
+        
+            {
+            balance = amount;
+
+            }
+
+        public BankAccount()
+        {
+        }
+
+        private double balance = 0;
+        
+
+        public void Deposit (double amount)
+        {
+            balance += amount;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+        
+
+         
+    }   
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myAccount = new BankAccount(500000.00);
+            Console.WriteLine("New bank account created");
+            Console.WriteLine($"Your balance is: {myAccount.GetBalance()}");
+
+            var secondAccount = new BankAccount();
+            Console.WriteLine("2nd account created");
+            Console.WriteLine($"Your 2nd account balance is: {secondAccount.GetBalance()}");
+
+            secondAccount.Deposit(1000000.00);
+            Console.WriteLine("Deposit accepted.");
+            Console.Write($"New balance is: {secondAccount.GetBalance()}");
+
+
+         }
+    }
+}
